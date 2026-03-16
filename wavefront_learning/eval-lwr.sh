e9492fb6aa21fb2f274532724fd126bb74e3165c
mchami <chamim@ethz.ch>
2026-03-16 11:38:38 -0700

    cleaner

--- wavefront_learning/eval-lwr.sh
+++ wavefront_learning/eval-lwr.sh
@@ -12,20 +12,22 @@ plotting="grid_residual"
 exp="lwr-experiments"
 
 # models
-all_models=("FNO" "DeepONet" "WaveNOBare" "Godunov" "WaveNO")
-ablation_models=("WaveNOBare" "WaveNO")
+all_models=("FNO" "DeepONet" "WaveNOBiasOnly" "WaveNOBare" "Godunov" "WaveNO")
+ablation_models=("WaveNOBiasOnly" "WaveNOBare" "WaveNO")
 loss_fn="mse"
 
 # ----------------- #
+exp_models=("${all_models[@]}") 
+
 # Testing it runs
-for model in "${ablation_models[@]}"; do
+for model in "${exp_models[@]}"; do
     uv run train.py --model $model --n_samples $n_samples --epochs $test_epochs --loss $loss_fn --no_wandb --seed ${seeds[0]}
 done
 
 # LWR experiments
 for i in $(seq 0 $((n_runs - 1))); do
     seed=${seeds[$i]}
-    for model in "${ablation_models[@]}"; do
+    for model in "${exp_models[@]}"; do
         uv run train.py --model $model --n_samples $n_samples --epochs $train_epochs --loss $loss_fn --plot $plotting --exp $exp --run_name $model --seed $seed
     done
 done
